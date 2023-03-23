16b8390117f4e647a7665e305ef308e46a1ea575
robertnelson-co <hello@robertnelson.co>
2023-03-23 18:49:36 -0400

    Uploading everything for the final assessment of the intro course

---
+++ script.js
@@ -0,0 +1,15 @@
+// Obtain the img element, and assign it to the image variable
+let image = document.querySelector('#pet')
+
+// Obtain the 'shrink-grow' button, and assign it to the changeSize variable
+let changeSize = document.querySelector('#shrink-grow')
+
+//DO NOT CHANGE THE FOLLOWING CODE
+changeSize.addEventListener("click", function() {
+  if (image.style.height == "100px") {
+    image.style.height = "25px"
+  }
+  else {
+    image.style.height = "100px"
+  }
+})
